08ada4a56d170734b16eddd539346075b203b0fb
Allan Carvalho <allandcarv@gmail.com>
2018-12-17 13:14:05 -0200

    Lista e busca de lançamentos

---
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
@@ -0,0 +1,35 @@
+package com.algaworks.algamoney.api.resource;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algamoney.api.model.Lancamento;
+import com.algaworks.algamoney.api.repository.LancamentoRepository;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+	@GetMapping
+	public List<Lancamento> listarLancamentos() {
+		return lancamentoRepository.findAll();
+	}
+	
+	@GetMapping("/{codigo}") 
+	public ResponseEntity<Lancamento> listarPeloCodigo(@PathVariable Long codigo) {
+		Lancamento lancamento = lancamentoRepository.findOne(codigo);
+		
+		return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+	}
+}
