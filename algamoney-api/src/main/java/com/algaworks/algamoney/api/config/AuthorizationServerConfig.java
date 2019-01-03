41762d49dbe91723ec25d823b1f3032d6f0df9f7
Allan Carvalho <allandcarv@gmail.com>
2019-01-03 21:21:53 -0200

    Implementando projecao de lancamento

--- algamoney-api/src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -26,14 +26,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.secret("@ngul@r0")
 				.scopes("read", "write")
 				.authorizedGrantTypes("password", "refresh_token")
-				.accessTokenValiditySeconds(20)
+				.accessTokenValiditySeconds(1800)
 				.refreshTokenValiditySeconds(3600 * 24) // 1 day
 			.and()
 				.withClient("mobile")
 				.secret("m0b1l30")
 				.scopes("read")
 				.authorizedGrantTypes("password", "refresh_token")
-				.accessTokenValiditySeconds(20)
+				.accessTokenValiditySeconds(1800)
 				.refreshTokenValiditySeconds(3600 * 24); // 1 day
 	}
 	
